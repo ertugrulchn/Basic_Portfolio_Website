6532dccfe7a26d30f4a2843b0fc73e5f4fde309b
ertugrulchn <cihanertugrulemre368@gmail.com>
2022-05-07 12:16:02 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// Type Writer Effect
+const typeWriter = (selector) => {
+  const el = document.querySelector(selector);
+  const text = el.innerHTML;
+
+  (function _type(i = 0) {
+    if (i === text.length) return;
+
+    el.innerHTML =
+      text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+    setTimeout(() => _type(i + 1), 100);
+  })();
+};
+
+typeWriter(".type_effetct");
